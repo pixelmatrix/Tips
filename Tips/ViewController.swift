ca097445b5deb0f940f91c907496f12195af9ed7
Josh Pyles <joshpyles@gmail.com>
2015-02-20 18:29:31 -0800

    Added gestureRecognizers
    Added support for tapping and swiping on the field to show/hide the
    keyboard

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -25,21 +25,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var totalSplit3View: UIView!
     @IBOutlet weak var totalSplit4View: UIView!
     
-    private var splitViews: NSArray!
+    private var splitViews: [UIView]!
     private var kbHeight: CGFloat!
+    private var center: CGFloat!
     
+    private var editActive: Bool = false
     private var currentString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Capture keyboard events
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
         
+        // Set field delegate
         billField.delegate = self
         
+        // Set initial values for text fields
         tipLabel.text = "+$0.00"
         totalLabel.text = "$0.00"
         totalSplit2Label.text = "$0.00"
@@ -47,15 +52,104 @@ class ViewController: UIViewController, UITextFieldDelegate {
         totalSplit4Label.text = "$0.00"
         billField.text = "$0.00"
         
+        // Set initial style for labels
         totalSplit2View.alpha = 0
         totalSplit3View.alpha = 0
         totalSplit4View.alpha = 0
         tipAmount.alpha = 0
         
+        // Store views
         splitViews = [totalSplit2View, totalSplit3View, totalSplit4View]
         
+        // Set-up gesture recognizers
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: "startEditing")
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.numberOfTouchesRequired = 1
+        containerView.addGestureRecognizer(tapRecognizer)
+        
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: "handleDrag:")
+        containerView.addGestureRecognizer(panRecognizer)
         // Focus field
+        startEditing()
+    }
+    
+    func startEditing() {
         billField.becomeFirstResponder()
+        self.editActive = true
+    }
+    
+    func handleDrag(pan: UIPanGestureRecognizer) {
+        
+        let translation = pan.translationInView(pan.view!)
+        
+        // Calculate distance of swipe relative to size of containerView
+        let d: CGFloat = (translation.y / pan.view!.frame.height) * 2.0
+
+        switch pan.state {
+        case .Began:
+            break
+        case .Changed:
+            // Update our transition
+            if d < 0 && self.editActive == true {
+                // Swiping out
+                if (d < -1.5) {
+                    endEditing() // Force complete at 150%
+                } else {
+                    transitionToEditMode(d, reverse: true)
+                }
+            } else if d > 0 && self.editActive == false {
+                // Swiping in
+                if d > 1.5 {
+                    startEditing() // Force complete at 150%
+                } else {
+                    transitionToEditMode(d, reverse: false)
+                }
+            }
+            break
+        default:
+            // Finish our transition
+            if (d > 0.5) {
+                startEditing()
+            } else {
+                endEditing()
+            }
+            break
+        }
+    }
+    
+    func transitionToEditMode(percentage: CGFloat, reverse: Bool) {
+        var p = percentage
+        
+        if reverse == true {
+            p = p * -1
+        }
+        
+        var h:CGFloat = 0.0
+        var s:CGFloat = 0.0
+        var b:CGFloat = 0.0
+        var a:CGFloat = 0.0
+        
+        let startColor = UIColor.groupTableViewBackgroundColor()
+        startColor.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+        
+        
+        let s2 = reverse ? s * p : s - (s * p)
+        let b2 = reverse ? (b-1) * p + 1 : ((1-b) * p) + b
+        
+        let editPos = (self.center - (self.containerView.frame.height / 2))
+        
+        let currentPos = reverse ? (0-editPos) * p + editPos : editPos * p
+        
+        containerView.transform = CGAffineTransformMakeTranslation(0, currentPos)
+        containerView.backgroundColor = UIColor(hue: h, saturation: s2, brightness: b2, alpha: 1.0)
+        
+        let currentAlpha = reverse ? p : 1 - p
+        
+        tipAmount.alpha = currentAlpha
+        
+        for view in splitViews {
+            view.alpha = currentAlpha
+        }
     }
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
@@ -128,8 +222,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         if let userInfo = sender.userInfo {
             if let keyboardHeight = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue().size.height {
-                let center = (viewHeight - keyboardHeight)/2
-                self.containerView.transform = CGAffineTransformMakeTranslation(0, center-(containerHeight/2))
+                self.center = (viewHeight - keyboardHeight)/2
+                self.containerView.transform = CGAffineTransformMakeTranslation(0, self.center-(containerHeight/2))
             }
         }
     }
@@ -156,6 +250,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func onEditingEnded(sender: AnyObject) {
+        endEditing()
+    }
+    
+    func endEditing() {
+        view.endEditing(true)
         UIView.animateWithDuration(0.3, animations: {
             self.containerView.backgroundColor = UIColor.groupTableViewBackgroundColor()
             self.containerView.transform = CGAffineTransformIdentity
@@ -169,15 +268,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
             
             UIView.animateWithDuration(0.3, delay: NSTimeInterval(delay), options: nil, animations: {
                 label.alpha = 1
-            }, completion: nil)
+                }, completion: nil)
             
         }
-        
-        
+        self.editActive = false
     }
     
     @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
+        endEditing()
     }
 }
 
